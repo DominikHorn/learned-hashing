8163d4971fd5dcb981a8ae4f830676982b85282b
Dominik Horn <dominik.horn@tum.de>
2021-08-07 13:03:29 +0200

    implement proper benchmark

--- src/benchmarks.cpp
+++ src/benchmarks.cpp
@@ -2,13 +2,24 @@
 
 #include <cstdint>
 #include <learned_hashing.hpp>
+#include <random>
 
 template <class Hashfn>
 static void BM_LearnedHash(benchmark::State& state) {
   using Data = std::uint64_t;
-  std::vector<Data> dataset{1, 2, 3, 5, 6, 7, 8};
+  std::vector<Data> dataset(100000000);
+  for (Data i = 0; i < dataset.size(); i++) dataset[i] = i + 20000;
 
-  Hashfn hashfn(dataset.begin(), dataset.end(), dataset.size());
+  std::random_device rd;
+  std::default_random_engine rng(rd());
+  std::uniform_int_distribution<size_t> dist(0, dataset.size() - 1);
+  std::vector<Data> sample(dataset.size() / 100);
+  for (size_t i = 0; i < sample.size(); i++) sample[i] = dataset[dist(rng)];
+  std::sort(sample.begin(), sample.end());
+  sample.erase(std::unique(sample.begin(), sample.end()), sample.end());
+  sample.shrink_to_fit();
+
+  Hashfn hashfn(sample.begin(), sample.end(), dataset.size());
 
   for (auto _ : state) {
     for (const auto& key : dataset) {
@@ -27,6 +38,6 @@ BENCHMARK_TEMPLATE(BM_LearnedHash,
 BENCHMARK_TEMPLATE(BM_LearnedHash,
                    learned_hashing::RadixSplineHash<std::uint64_t>);
 BENCHMARK_TEMPLATE(BM_LearnedHash,
-                   learned_hashing::RMIHash<std::uint64_t, 100>);
+                   learned_hashing::RMIHash<std::uint64_t, 1000>);
 
 BENCHMARK_MAIN();
