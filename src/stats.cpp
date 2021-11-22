a354227e18c63f161d6f7c4eddbddd083586abdb
Dominik Horn <dominik.horn@tum.de>
2021-11-22 13:45:27 +0100

    implement hist .csv export

--- src/stats.cpp
+++ src/stats.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <cstdint>
+#include <fstream>
 #include <iomanip>
 #include <ios>
 #include <iostream>
@@ -19,13 +20,13 @@
  */
 template <class Hashfn, class RandomIt>
 std::vector<size_t> histogram(const RandomIt& begin, const RandomIt& end,
-                              double bucket_size) {
+                              double bucket_step) {
   // preconditions
   std::is_sorted(begin, end);
 
   // variables
   const auto dataset_size = std::distance(begin, end);
-  const auto bucket_cnt = 1.0 / bucket_size;
+  const auto bucket_cnt = 1.0 / bucket_step;
   std::vector<size_t> buckets(bucket_cnt, 0);
 
   // train hash function
@@ -46,8 +47,8 @@ void print_hist(const std::vector<size_t>& hist, double bucket_step,
             << " elems):" << std::endl;
   for (size_t i = 0; i < hist.size(); i++) {
     const auto bucket = hist[i];
-    std::cout << "[" << i * bucket_step << ", " << (i + 1) * bucket_step
-              << "): ";
+    std::cout << "[" << i * bucket_step << ", " << (i + 1) * bucket_step << ") "
+              << bucket << " elems: ";
     for (size_t i = 0; i < (100 * bucket / *max); i++) {
       std::cout << "*";
     }
@@ -55,13 +56,23 @@ void print_hist(const std::vector<size_t>& hist, double bucket_step,
   }
 }
 
-int main() {
-  using RMI = learned_hashing::RMIHash<std::uint64_t, 1000000>;
-  using MonotoneRMI = learned_hashing::MonotoneRMIHash<std::uint64_t, 1000000>;
+void hist_to_csv(const std::string& filepath, const std::vector<size_t>& hist,
+                 double bucket_step) {
+  if (hist.empty()) return;
 
-  const auto dataset_size = 10000000;
-  const auto bucket_step = 0.02;
+  std::ofstream csv_file;
+  csv_file.open(filepath);
 
+  csv_file << "bucket_lower,bucket_upper,bucket_value" << std::endl;
+  for (size_t i = 0; i < hist.size(); i++)
+    csv_file << i * bucket_step << "," << (i + 1) * bucket_step << ","
+             << hist[i] << std::endl;
+
+  csv_file.close();
+}
+
+template <class HashFn>
+void hist_all_ds(size_t dataset_size = 10000000, double bucket_step = 0.001) {
   for (const auto did :
        {dataset::ID::SEQUENTIAL, dataset::ID::GAPPED_10, dataset::ID::UNIFORM,
         dataset::ID::WIKI, dataset::ID::NORMAL, dataset::ID::OSM,
@@ -69,17 +80,23 @@ int main() {
     const auto dataset = dataset::load_cached(did, dataset_size);
     if (dataset.empty()) continue;
 
-    const auto rmi_hist =
-        histogram<RMI>(dataset.begin(), dataset.end(), bucket_step);
-    const auto monotone_rmi_hist =
-        histogram<MonotoneRMI>(dataset.begin(), dataset.end(), bucket_step);
+    const auto hist =
+        histogram<HashFn>(dataset.begin(), dataset.end(), bucket_step);
 
-    // TODO: tmp
-    std::cout << " ===== RMI ===== " << std::endl;
-    print_hist(rmi_hist, bucket_step, did, dataset);
-    std::cout << " ===== MONOTONE RMI ===== " << std::endl;
-    print_hist(monotone_rmi_hist, bucket_step, did, dataset);
+    hist_to_csv(HashFn::name() + "_" + dataset::name(did) + ".csv", hist,
+                bucket_step);
+
+    // std::cout << "===== " << HashFn::name() << " =====" << std::endl;
+    // print_hist(hist, bucket_step, did, dataset);
   }
+}
+
+int main() {
+  using RMI = learned_hashing::RMIHash<std::uint64_t, 1000000>;
+  using MonotoneRMI = learned_hashing::MonotoneRMIHash<std::uint64_t, 1000000>;
+
+  hist_all_ds<RMI>();
+  hist_all_ds<MonotoneRMI>();
 
   return 0;
 }
