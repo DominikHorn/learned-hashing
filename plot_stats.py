f0a641bde234fe5323cdc54c3e02a7b146b33105
Dominik Horn <dominik.horn@tum.de>
2021-11-22 14:36:42 +0100

    print total bucket count in hist fig title

--- plot_stats.py
+++ plot_stats.py
@@ -8,5 +8,5 @@ if len(sys.argv) < 2:
 
 for ds in sys.argv[1:]:
     df = pd.read_csv(ds)
-    fig = px.line(df, x="bucket_lower", y="bucket_value", title=ds)
+    fig = px.line(df, x="bucket_lower", y="bucket_value", title=f"{ds} ({len(df)} buckets)")
     fig.show()
